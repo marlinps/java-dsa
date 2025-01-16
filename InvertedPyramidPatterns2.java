4d39e9818c86ae28821ad9f9c00c920851488afd
marlinps <marlinpurnamas92@gmail.com>
2025-01-16 13:31:13 +0700

    updated inverted half pyramid

--- InvertedPyramidPatterns2.java
+++ InvertedPyramidPatterns2.java
@@ -6,20 +6,18 @@ public class InvertedPyramidPatterns2{
         int n = sc.nextInt();
 
         //outer loop
-        for(int i=14; i<=n; i++){
+        for(int i=1; i<=n; i++){
             //TODO: inner loop -> space print
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
-
             //TODO: inner loop -> star print
             for(int j=1 ; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
         }   
-    }
-    
+    }  
 }
 
 /* TODO: Example Output Patterns rotated by 180 deg
