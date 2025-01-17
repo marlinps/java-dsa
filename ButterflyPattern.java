cb9e46e04e5f757e4a8bcd670ca77f34bb538bc8
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-01-17 23:42:10 +0700

    update butterfly pattern

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -24,13 +24,13 @@ public class ButterflyPattern{
         }
 
          // Lower Half
-         for(int i=n; i<=1; i--){
+         for(int i=n; i>=1; i--){
             //1st part
             for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
             //spaces
-            int spaces = 2*(n-i);
+            int spaces = 2 * (n-i);
             for(int j=1; j<=spaces; j++){
                 System.out.print(" ");
             }
