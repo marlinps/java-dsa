ab08f1efb6509f9c53cbcc3ca01bf1592ed983f7
marlinps <marlinpurnamas92@gmail.com>
2025-01-23 10:35:01 +0700

    sum all odd numbers function

---
+++ SumAllOddNumbersFunction.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class SumAllOddNumbersFunction {
+    public static void CalculateSumAllNumbers(int n){
+        int sum = 0;
+
+        if(n < 0){
+            System.out.println("Invalid Numbers");
+        }else{
+            for(int i=1; i<=n; i++){
+                if (i%2 != 0 ){
+                    sum += i;
+                }
+            }
+            System.out.println("Sum of n odd numbers "+sum);
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        CalculateSumAllNumbers(n);
+
+    }
+    
+}
