d48a43c524aaf295663a2483c3dad18f31047dfd
marlinps <marlinpurnamas92@gmail.com>
2025-01-23 15:10:48 +0700

    create arrays2

---
+++ Arrays2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Arrays2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        //input value arrays
+        for(int i=0; i<size; i++){
+            numbers[i] = sc.nextInt();
+        }
+
+        //output
+        for(int i=0; i<size; i++){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
