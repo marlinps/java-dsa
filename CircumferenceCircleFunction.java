f0eeb5b6bd45bd7eae32353b2bf628baccf732ca
marlinps <marlinpurnamas92@gmail.com>
2025-01-23 11:12:03 +0700

    create circumferencee cicle function

---
+++ CircumferenceCircleFunction.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CircumferenceCircleFunction {
+    public static double CircumferenceCircle(int r){
+        double pi = 3.14;
+
+        double c = 2 * pi *r;
+        return c;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+
+        System.out.println("Circumference of a circle "+CircumferenceCircle(r));
+    }
+}
