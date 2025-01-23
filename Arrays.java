53e9c6e88c63220f552d386b18c1455893d7a68b
marlinps <marlinpurnamas92@gmail.com>
2025-01-23 14:59:24 +0700

    update arrays

--- Arrays.java
+++ Arrays.java
@@ -1,12 +1,12 @@
 import java.util.*;
-
 public class Arrays {
     public static void main(String[] args){
         // int[] marks = new int[3];
-        int marks[] = new int[3];
-        marks[0] = 97;
-        marks[1] = 98;
-        marks[2] = 95;
+        // int marks[] = new int[3];
+        // marks[0] = 97; //phy
+        // marks[1] = 98; //chem
+        // marks[2] = 95; //eng
+        int marks[] = {97, 98, 95};
 
         // System.out.println(marks[0]);
         // System.out.println(marks[1]);
