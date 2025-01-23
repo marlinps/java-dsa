a34b71915fc4e0b9e58b758f10f57bbe770c238f
marlinps <marlinpurnamas92@gmail.com>
2025-01-23 11:30:15 +0700

    create eligible vote function

---
+++ EligibleVoteFunction.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class EligibleVoteFunction {
+    public static String EligibleVoteFunction(int age){
+        String status;
+
+        if (age > 18){
+            status = "Eligible to Vote";
+        }else{
+            status = "Not";
+        }
+
+        return status;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        System.out.println("A person of age "+age+" is "+EligibleVoteFunction(age));
+    }
+}
