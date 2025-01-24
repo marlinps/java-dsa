c341d77927ac1ca886df2570ba8eeaa5a8913d2d
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-01-24 20:04:44 +0700

    update inverted pyramid patterns2

--- InvertedPyramidPatterns2.java
+++ InvertedPyramidPatterns2.java
@@ -11,10 +11,12 @@ public class InvertedPyramidPatterns2{
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
+            
             //TODO: inner loop -> star print
             for(int j=1 ; j<=i; j++){
                 System.out.print("*");
             }
+
             System.out.println();
         }   
     }  
