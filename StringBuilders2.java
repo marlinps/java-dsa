a4469d90accbaaf3df57a90953b17aa96733d4f5
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-02-03 21:08:16 +0700

    create append string builder

---
+++ StringBuilders2.java
@@ -0,0 +1,12 @@
+public class StringBuilders2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e"); //str = str + "e";
+        sb.append("l"); //str = str + "l";
+        sb.append("l"); //str = str + "l";
+        sb.append("o"); //str = str + "o";
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+    
+}
