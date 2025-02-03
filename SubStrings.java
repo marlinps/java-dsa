ec5bd9f2b7fe28cbdc3856ad2bf2abd5f512a8c7
marlinps <marlinpurnamas92@gmail.com>
2025-02-03 14:30:25 +0700

    create substrings

---
+++ SubStrings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SubStrings {
+    public static void main(String[] args) {
+        String sentence = "TonyStark";
+        String name1 = sentence.substring(0,4);
+        String name2 = sentence.substring(4);
+
+        System.out.println(name1);
+        System.out.println(name2);
+    }
+}
