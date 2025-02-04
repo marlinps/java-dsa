3a03430f278736dbb527e6ccfead6c6d5d02ec71
marlinps <marlinpurnamas92@gmail.com>
2025-02-04 14:58:35 +0700

    create Operator Decrements (Pre and Post)

--- OperatorDecrements.java
+++ OperatorDecrements.java
@@ -1,20 +1,20 @@
 import java.util.*;
 
-public class OperatorIncrements {
+public class OperatorDecrements {
     public static void main(String[] args) {
-        //TODO: Pre Increment ++a 
+        //TODO: Pre Decrement --a
         int a = 10;
         int b = 0;
 
-        b = ++a;
+        b = --a;
         System.out.println(a);
         System.out.println(b);
 
-        //TODO: Post Increment a++
+        //TODO: Post Decrement a--
         int a1 = 10;
         int b1 = 0;
 
-        b1 = a1++;
+        b1 = a1--;
         System.out.println(a1);
         System.out.println(b1);
     }
