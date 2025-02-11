47fdc2fe94a78196f640042fa15823018fab9c3e
marlinps <marlinpurnamas92@gmail.com>
2025-02-11 10:23:19 +0700

    update recursionsumnumbers

--- RecursioSumNumbers.java
+++ RecursioSumNumbers.java
@@ -8,7 +8,9 @@ public class RecursioSumNumbers {
 
         sum += i;
         printSum(i+1, n, sum);
+        System.out.println(i);
     }
+
     public static void main(String[] args) {
         printSum(1, 5, 0);
     }
