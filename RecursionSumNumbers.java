f30b33a59f0350e27ba34949cd3ebb11ebb04f90
marlinps <marlinpurnamas92@gmail.com>
2025-02-11 14:39:36 +0700

    update reecursion sum numbers

--- RecursionSumNumbers.java
+++ RecursionSumNumbers.java
@@ -1,4 +1,4 @@
-public class RecursioSumNumbers {
+public class RecursionSumNumbers {
     public static void printSum(int i, int n, int sum){
         if(i == n){
             sum += i;
@@ -10,10 +10,8 @@ public class RecursioSumNumbers {
         printSum(i+1, n, sum);
         System.out.println(i);
     }
-
     public static void main(String[] args) {
         printSum(1, 5, 0);
     }
 }
-
 //TODO: Print sum of fiirst n natural numbers
