75520d7bf0c06d70c5d9c659df0af7b536fc2704
marlinps <marlinpurnamas92@gmail.com>
2025-04-11 14:56:05 +0700

    update For

--- For.java
+++ For.java
@@ -1,6 +1,6 @@
 class ForClass{
     public static void main(String[] args) {
-        //for
+        //TODO: for(initilisation; condition; updation)
         for(int i = 0; i < 11; i++){
             System.out.println(i);
         }        
