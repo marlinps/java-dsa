325e83fe49d92070eb3729d7c1372cf3dcb0c20a
marlinps <marlinpurnamas92@gmail.com>
2025-04-11 14:57:19 +0700

    update While

--- While.java
+++ While.java
@@ -2,6 +2,7 @@ class WhileClass{
     public static void main(String[] args) {
         int i = 0;
 
+        //TODO: while(condition)
         while(i < 11){
             System.out.println(i);
             i++;
