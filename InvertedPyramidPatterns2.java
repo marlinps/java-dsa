f2d64cf0c2e05f17e830dd97e0479054156f78f9
marlinps <marlinpurnamas92@gmail.com>
2025-04-11 15:50:26 +0700

    update InvertedPyramidPatterns2

--- InvertedPyramidPatterns2.java
+++ InvertedPyramidPatterns2.java
@@ -5,7 +5,7 @@ public class InvertedPyramidPatterns2{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        //outer loop
+        //TODO: outer loop
         for(int i=1; i<=n; i++){
             //TODO: inner loop -> space print
             for(int j=1; j<=n-i; j++){
