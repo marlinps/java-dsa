b82701091c78a7494824e2f487865c08eeff0820
marlinps <marlinpurnamas92@gmail.com>
2025-04-25 14:52:16 +0700

    create HollowButterflyPatterns

---
+++ HollwoButterflyPatterns.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class HollwoButterflyPatterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //upper
+        for (int i = 1; i <= n; i++) {
+            //1st half
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            //spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            //2nd half
+            for (int j = 1; j <= i; j++) {
+                if (j == i || j == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        //upper
+        for (int i = n; i >= 1; i--) {
+            //1st half
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            //spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            //2nd half
+            for (int j = 1; j <= i; j++) {
+                if (j == i || j == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+/* TODO: Example Output Patterns for n= 5
+*        *
+**      **
+* *    * *
+*  *  *  *
+*   **   *
+*   **   *
+*  *  *  *
+* *    * *
+**      **
+*        *
+*/
