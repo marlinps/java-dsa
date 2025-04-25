f27b49aca25bc472b3870cb023ffc267b7a3b375
marlinps <marlinpurnamas92@gmail.com>
2025-04-25 10:37:07 +0700

    update PascalsTrianglePatterns

--- PascalsTrianglePatterns.java
+++ PascalsTrianglePatterns.java
@@ -5,15 +5,34 @@ public class PascalsTrianglePatterns {
         Scanner sn = new Scanner(System.in);
         int n = sn.nextInt();
 
+        //outer loop
+        for(int i=1; i<=n; i++){
+            int c =1;
 
+            //print space
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            //print number
+            for(int j=1; j<=i; j++){
+                System.out.print(c+" ");
+                c = c * (i-j)/j;
+            }
+
+            System.out.println();
+        }
     }
-    
 }
 
-/* TODO:Example Output Patterns n= 5
+/*TODO: Example Output Patterns n= 5
     1
-   1 2
-  1 2 3
- 1 2 3 4
-1 2 3 4 5
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+
+TODO: this is binomial coefficent 
 */
+
+
