3f9352c29a9b09f544727af6bae84a2cd8ab98fc
marlinps <marlinpurnamas92@gmail.com>
2025-04-28 10:13:54 +0700

    update HollowButterflyPatterns

--- HollowButterflyPatterns.java
+++ HollowButterflyPatterns.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HollwoButterflyPatterns {
+public class HollowButterflyPatterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
