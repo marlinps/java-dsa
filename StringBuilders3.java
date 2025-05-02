654da1c833ab0a30a40b31dad88629774942bfdb
marlinps <marlinpurnamas92@gmail.com>
2025-05-02 13:54:01 +0700

    create StringBuilders3"

---
+++ StringBuilders3.java
@@ -0,0 +1,11 @@
+public class StringBuilders3 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Marlin");
+        System.out.println("Intial StringBuilder : " + sb);
+
+        sb.append(" Purnama Sari");
+        System.out.println("After append: " + sb);
+
+        System.out.println(sb.length());
+    }
+}
