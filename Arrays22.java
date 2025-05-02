9750c1d05045ae2a3c241ea742206ee487c53ffe
marlinps <marlinpurnamas92@gmail.com>
2025-05-02 13:36:06 +0700

    update Arrayys22

--- Arrays22.java
+++ Arrays22.java
@@ -7,18 +7,21 @@ public class Arrays22 {
         int m = sc.nextInt();//columns
         int numbers[][] = new int[n][m];
 
-        //input value arrays
+        //input 
+        //rows
         for(int i=0; i<n; i++){
+            //columns
             for(int j=0; j<m; j++){
                 numbers[i][j] = sc.nextInt();
             }
         }
 
-        // //output
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<m; j++){
-        //         System.out.println(numbers[i][j]);
-        //     }
-        // }
+        //output
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.println(numbers[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
