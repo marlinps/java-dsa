c7d116bd2ea90ffb7eec487434c03e7fa351753f
marlinps <marlinpurnamas92@gmail.com>
2025-05-02 13:16:06 +0700

    create TimeComplexity

---
+++ TimeComplexity.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TimeComplexity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //Input n
+
+        for(int i=0; i<=n; i++){//n times
+            System.out.println("Hello");
+        }
+    }
+}
+
+/* TODO: Notes
+ * Relation between input size & running time (operation)
+ */
