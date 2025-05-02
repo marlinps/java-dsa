2290f0e6c427ebf1179179bcfd9a93dc7c26d5aa
marlinps <marlinpurnamas92@gmail.com>
2025-05-02 13:33:02 +0700

    createe Arrayys22

---
+++ Arrays22.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Arrays22 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();//rows
+        int m = sc.nextInt();//columns
+        int numbers[][] = new int[n][m];
+
+        //input value arrays
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        // //output
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         System.out.println(numbers[i][j]);
+        //     }
+        // }
+    }
+}
