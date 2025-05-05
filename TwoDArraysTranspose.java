1ef02187f978eb468518f387f0471bab55a8156a
marlinps <marlinpurnamas92@gmail.com>
2025-05-05 11:36:04 +0700

    create TwoDArraysTranspose

---
+++ TwoDArraysTranspose.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+
+public class TwoDArraysTranspose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        //input
+        //rows
+        for(int i=0; i<rows; i++){
+            //columns
+            for(int j=0; j<cols; j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        //ouput
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }   
+        
+        //to print tranpose
+        for(int j=0; j<cols; j++){
+            for(int i=0; i<rows; i++){
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/* TODO: Example 
+Input : rows = 2, cols= 3
+1 2 3
+2 3 4
+
+Output
+1 2
+2 3
+3 4
+ */
