5580e0251a15e49aec573e096489d1d5fc59f1cd
marlinps <marlinpurnamas92@gmail.com>
2025-05-05 14:45:21 +0700

    update BubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -7,11 +7,11 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {7,8,3,1,2};
+        int arr[] = {7,8,3,1,2,9};
         
         //TODO: time complexity = 0(n^2)
         //TODO: bubble sort
-        for(int i=0; i<arr.length-1; i++){// n-1 -> 5-1=4
+        for(int i=0; i<arr.length-1; i++){//n-1 -> 5-1=4
             for(int j=0; j<arr.length-i-1; j++){ //n-i-1 -> n-0-1=4-0-1=3
                 if(arr[j] > arr[j+1]){
                     //TODO: swap
