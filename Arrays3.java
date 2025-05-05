a02bba8ca5ef006c181984cc25c6c4d7928b15f4
marlinps <marlinpurnamas92@gmail.com>
2025-05-05 10:49:43 +0700

    create Arrays3

---
+++ Arrays3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+//TODO: Linear Search
+public class Arrays3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        //input
+        for(int i=0; i<numbers.length; i++){
+            numbers[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        //output
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i] == x){
+                System.out.println("x: " + x + " found is index: " + i);
+            }
+        }
+    }
+}
+
