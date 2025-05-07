539178b44ea8924d890c03599e58b9c323fffe16
marlinps <marlinpurnamas92@gmail.com>
2025-05-07 14:58:57 +0700

    update RecursionFibonacci

--- Recursion2towerOfHanoi.java
+++ Recursion2towerOfHanoi.java
@@ -12,7 +12,23 @@ public class Recursion2towerOfHanoi {
     }
 
     public static void main(String[] args) {
-        int n = 3;
+        int n = 2;
         towerOfHanoi(n, "S", "H", "D");
     }
 }
+
+/* TODO: TOWER OF HANOI ALGORITHM
+ *  TOH(n, S, H, D)
+ *  1. TOH(n-1, S, D, H)
+ *  2. Move disc n from S to C
+ *  3. TOH(n-1, H, S, D)
+ */
+
+ /* TODO: NOTES
+    The idea is to use the helper node to reach the destination using recursion. Below is the pattern for this problem:
+
+    Shift ‘N-1’ disks from ‘S’ to ‘D’, using H.
+    Shift last disk from ‘S’ to ‘D’.
+    Shift ‘N-1’ disks from ‘H’ to ‘D’, using S.
+  
+  */
