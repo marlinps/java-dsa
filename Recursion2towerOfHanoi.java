0fac961656ed0ae09da2f4b71bb860454f158d64
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-08 10:34:52 +0700

    update Recursion2towerOfHanoi

--- Recursion2towerOfHanoi.java
+++ Recursion2towerOfHanoi.java
@@ -24,7 +24,13 @@ public class Recursion2towerOfHanoi {
  *  3. TOH(n-1, H, S, D)
  */
 
- /* TODO: NOTES
+ /* TODO: FORMULA
+    The recursive solution to this problem can be broken down into three steps:
+    
+    1. Move n-1 disks from source to helper using destination as a helper.
+    2. Move the nth disk from source to destination.
+    3. Move n-1 disks from helper to destination using source as a helper.
+
     The idea is to use the helper node to reach the destination using recursion. Below is the pattern for this problem:
 
     Shift ‘N-1’ disks from ‘S’ to ‘D’, using H.
