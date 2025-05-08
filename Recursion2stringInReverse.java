2ae62a3554219bc61ce92299e040eccb9e5f13be
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-08 11:19:03 +0700

    create Recursion2stringInReverse

---
+++ Recursion2stringInReverse.java
@@ -0,0 +1,22 @@
+public class Recursion2stringInReverse {
+    public static void printRev(String str, int idx){
+        //TODO: Base case
+        if(idx == 0){
+            System.out.print(str.charAt(idx));
+            return;
+        }
+
+        System.out.print(str.charAt(idx));
+        //TODO: recursive call
+        printRev(str, idx-1);
+      
+    }
+    public static void main(String[] args) {
+        String str = "abcd";
+        printRev(str, str.length()-1);  
+    }
+}
+
+/* TODO: String in Reverse exmp abcd -> dcba
+ * 
+ */
