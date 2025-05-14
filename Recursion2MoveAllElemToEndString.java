2ddf1f4d58f1a26db0d05573a2b9d9ec06bc76d4
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-14 14:34:46 +0700

    update Recursion2MoveAllElemToEndString

--- Recursion2MoveAllElemToEndString.java
+++ Recursion2MoveAllElemToEndString.java
@@ -1,6 +1,29 @@
 public class Recursion2MoveAllElemToEndString {
+    public static void moveAllX(String str, int idx, int count, String newString){
+        if(idx == str.length()){
+            //base case
+            for(int i = 0; i < count; i++){
+                newString += 'x'; //newString = newString + 'x'
+            }
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        if(currChar == 'x'){
+            count++;
+            //recursive call
+            moveAllX(str, idx + 1, count, newString);
+        }else{
+            newString += currChar; //newString = newString + currCHar
+            //recursive call
+            moveAllX(str, idx + 1, count, newString);
+        }
+    }
+
     public static void main(String[] args) {
-        
+        String str = "axbcxxd";
+        moveAllX(str, 0, 0, "");// 0 is the index of the string 
     }
 }
 
