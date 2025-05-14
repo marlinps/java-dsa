56d9765bb2a9327c1a2313d465445f2982ca7cbf
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-14 14:08:15 +0700

    update Recursion2ArrayIsSortedStrictlyIncreasing

--- Recursion2ArrayIsSortedStrictlyIncreasing.java
+++ Recursion2ArrayIsSortedStrictlyIncreasing.java
@@ -1,6 +1,21 @@
 public class Recursion2ArrayIsSortedStrictlyIncreasing {
+    public static boolean isSorted(int arr[], int idx){
+        //base case
+        if(idx == arr.length -1){
+            return true;
+        }
+
+        if(arr[idx] < arr[idx+1]){
+            //array is sorted till now
+            return isSorted(arr, idx + 1);
+        }else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        int arr[] = {1, 3, 3};
+        System.out.println(isSorted(arr, 0));
     }
 }
 
