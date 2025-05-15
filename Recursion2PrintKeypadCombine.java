0751877e5e874ce91290dffae593e16ed0a44d28
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-15 15:59:30 +0700

    update Recursion2PrintKeypadCombine

--- Recursion2PrintKeypadCombine.java
+++ Recursion2PrintKeypadCombine.java
@@ -1,12 +1,24 @@
 public class Recursion2PrintKeypadCombine {
     public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "xwx", "yz"};
 
-    public static printComb(String str, int idx, String combination){
+    public static void printComb(String str, int idx, String combination){
+        //Base case
+        if(idx == str.length()){
+            System.out.println(combination);
+            return;
+        }
 
+        char currChar = str.charAt(idx);
+        String mapping = keypad[currChar - '0'];
+
+        for(int i = 0; i < mapping.length(); i++){
+            printComb(str, idx + 1, combination + mapping.charAt(i));
+        }
     }
 
     public static void main(String[] args) {
-        
+        String str = "23";
+        printComb(str, 0, "");  
     }
 }
 
