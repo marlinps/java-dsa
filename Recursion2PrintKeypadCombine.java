6e762bbbde7b04866ec788524ad0e91f8e25b769
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-15 15:03:21 +0700

    create Recursion2PrintKeypadCombine

--- Recursion2PrintKeypadCombine.java
+++ Recursion2PrintKeypadCombine.java
@@ -1,5 +1,18 @@
 public class Recursion2PrintKeypadCombine {
+    public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "xwx", "yz"};
+
+    public static printComb(String str, int idx, String combination){
+
+    }
+
     public static void main(String[] args) {
         
     }
 }
+
+/*
+ * TODO: Print all combinations of a string
+ * 1. Base case: if the index is equal to the length of the string, print the current combination.  
+ * 2. Recursive case: if the index is less than the length of the string, print the current combination and call the function again with the index incremented by 1.
+ * 3. Call the function again with the index incremented by 1 and the current character added to the combination.
+ */
