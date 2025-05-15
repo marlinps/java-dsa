a86ed1d7dc8d41cf4fbdf928207290b7e2c6077b
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-15 13:51:01 +0700

    update Recursion2PrintAllSubseqString

--- Recursion2PrintAllSubseqString.java
+++ Recursion2PrintAllSubseqString.java
@@ -9,5 +9,7 @@ public class Recursion2PrintAllSubseqString {
     * 2. Recursive case: if the index is less than the length of the string, print the current subsequence and call the function again with the index incremented by 1. 
     * 3. Call the function again with the index incremented by 1 and the current character added to the subsequence.
     * 4. Print the current subsequence.
+    input: "abc"
+    * output: "", "a", "b", "ab", "c", "ac", "bc", "abc"
  * 
  */
