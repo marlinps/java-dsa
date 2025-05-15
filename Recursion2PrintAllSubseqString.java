dd0dbea7c1282f08c920d4b4e803cb9e68269b0f
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-15 14:07:11 +0700

    update Recursion2PrintAllSubseqString

--- Recursion2PrintAllSubseqString.java
+++ Recursion2PrintAllSubseqString.java
@@ -1,5 +1,24 @@
 public class Recursion2PrintAllSubseqString {
+    public static void subsequence(String str, int idx, String newString){
+        //Base case
+        if(idx == str.length()){
+           System.out.println(newString);
+           return;
+        }
+
+        char currChar = str.charAt(idx);
+
+        //to be
+        subsequence(str, idx + 1, newString + currChar); // 
+
+
+        //or not to be
+        subsequence(str, idx + 1, newString);
+    }
+
     public static void main(String[] args) {
+        String str = "abc";
+        subsequence(str, 0, "");
         
     }
 }
