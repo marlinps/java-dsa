6b24589d650b7858c1aef7915d2217ea39b026f4
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-15 14:38:20 +0700

    create Recursion2PrintAllUniqSubseqString

---
+++ Recursion2PrintAllUniqSubseqString.java
@@ -0,0 +1,18 @@
+public class Recursion2PrintAllUniqSubseqString {
+
+   
+
+    public static void main(String[] args) {
+        String str = "abc";
+       
+    }
+}
+
+/* TODO: Print all unique subsequences of a string
+  base case: if the index is equal to the length of the string, print the current subsequence.
+  recursive case: if the index is less than the length of the string, print the current subsequence and call the function again with the index incremented by 1. 
+  call the function again with the index incremented by 1 and the current character added to the subsequence.
+  print the current subsequence.
+
+ * 
+ */
