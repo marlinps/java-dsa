6086bab3155aa95d300a228f848d3596112998a1
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-15 13:49:39 +0700

    create Recursion2PrintAllSubseqString

---
+++ Recursion2PrintAllSubseqString.java
@@ -0,0 +1,13 @@
+public class Recursion2PrintAllSubseqString {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/* TODO: Print all subsequences of a string
+    * 1. Base case: if the index is equal to the length of the string, print the current subsequence.
+    * 2. Recursive case: if the index is less than the length of the string, print the current subsequence and call the function again with the index incremented by 1. 
+    * 3. Call the function again with the index incremented by 1 and the current character added to the subsequence.
+    * 4. Print the current subsequence.
+ * 
+ */
