d0ad276a9c6ab4b95827aa0e2838111eda9e87f5
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-20 11:26:17 +0700

    update Recursion3PrintAllSubsetsSetFirstNaturalNumbers

--- Recursion3PrintAllSubsetsSetFirstNaturalNumbers.java
+++ Recursion3PrintAllSubsetsSetFirstNaturalNumbers.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+
 public class Recursion3PrintAllSubsetsSetFirstNaturalNumbers {
+    public static void printSubsets(ArrayList<Integer> subset) {
+        for(int i=0; i < subset.size(); i++){
+            System.out.print(subset.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubsets(int n, ArrayList<Integer> subset) {
+        //Base case
+        if(n == 0){
+            printSubsets(subset);
+            return;
+        }
+
+        //Include the current number
+        subset.add(n);
+        findSubsets(n - 1, subset);
+
+        //Exclude the current number
+        subset.remove(subset.size() - 1);
+        findSubsets(n - 1, subset);
+    }
+    
     public static void main(String[] args) {
-        
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubsets(n, subset); 
     }
 }
 
