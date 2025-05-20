2385722d3ffa3a5538ee8e65291515d01319f280
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-20 09:10:38 +0700

    Recursion3PrintAllPemutationString

---
+++ Recursion3PrintAllPemutationString.java
@@ -0,0 +1,14 @@
+public class Recursion3PrintAllPemutationString {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+/* TODO: Print all permutations of a string "abc" using recursion.
+ * 
+ * input: "abc"
+ * output: "abc", "acb", "bac", "bca", "cab", "cba"
+ * 
+ */
--- RecursionFibonacci.java
+++ RecursionFibonacci.java
@@ -9,6 +9,7 @@ public class RecursionFibonacci {
         System.out.println(c);
         printFib(b, c, n-1);
     }
+
     public static void main(String[] args) {
         int a = 0, b = 1;
         System.out.println(a);
