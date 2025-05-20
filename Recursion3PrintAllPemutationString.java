e96bfc73b6e85ab1565f8d791d305ad8c36e6e2b
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-20 09:32:25 +0700

    update Recursion3PrintAllPemutationString

--- Recursion3PrintAllPemutationString.java
+++ Recursion3PrintAllPemutationString.java
@@ -1,8 +1,23 @@
 public class Recursion3PrintAllPemutationString {
+    public static void printPerm(String str, String permutation){
+        //Base Case
+        if(str.length() == 0){
+            System.out.println(permutation);
+            return;
+        }
 
+        for(int i=0; i<str.length(); i++){
+            char currChar = str.charAt(i);
+            //"abc" -> "bc"
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            //recursive call
+            printPerm(newStr, permutation + currChar);
+        }
+    }
 
     public static void main(String[] args) {
-        
+        String str = "abc";
+        printPerm(str, "");  
     }
 }
 
