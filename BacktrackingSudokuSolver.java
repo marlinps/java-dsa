dc1d2bd8c0cc7117d6bb51017b6a81840664804b
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-25 14:17:40 +0700

    update BacktrackingSudokuSolver

--- BacktrackingSudokuSolver.java
+++ BacktrackingSudokuSolver.java
@@ -11,7 +11,7 @@ public class BacktrackingSudokuSolver {
             }
         }
 
-        //grid
+        //grid(3x3)
         int sr = (row/3) * 3;
         int sc = (col/3) * 3;
 
@@ -61,7 +61,6 @@ public class BacktrackingSudokuSolver {
         }
 
         return false;
-
     }
 
     public void solveSudoku(char[][] board) {
@@ -90,17 +89,19 @@ public class BacktrackingSudokuSolver {
     }
 }
 
-/* Notes : Hard
- * Sudoku Solver
+/* TODO: Hard level problem
+ * TODO: Sudoku Solver 
+ * TODO: remember rule sudoku (1-9) in each row, column and 3x3 sub-box
+ 
  * Write a program to solve a Sudoku puzzle by filling the empty cells.
 
 A sudoku solution must satisfy all of the following rules:
 
-    Each of the digits 1-9 must occur exactly once in each row.
-    Each of the digits 1-9 must occur exactly once in each column.
-    Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
 
-The '.' character indicates empty cells.
+TODO: The '.' character indicates empty cells.
 
 Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
