50e889e520816626631f5ad26fb79225789a69ef
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-25 15:07:28 +0700

    update mergeSort (create divide function)

--- mergeSort.java
+++ mergeSort.java
@@ -1,4 +1,19 @@
 public class MergeSort {
+    public static void divide(int arr[], int si, int ei){
+        // Base case
+        // if start index is greater than or equal to end index, then return
+        if(si >= ei){
+            return;
+        }
+       
+        int mid = si + (ei - si)/2; //(si + ei)/2
+
+        //recursian call
+        divide(arr, si, mid);
+        divide(arr, mid+1, ei);
+        conquer(arr, si, mid, ei);
+    }
+
     public static void main(String[] args) {
         int arr[] = {6, 3,9 , 5, 2, 8};
         int n = arr.length;
