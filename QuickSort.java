cad9537ea51dc4ad29d8e7b304a69b2f9414a93d
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-27 14:44:18 +0700

    update  QuickSort

--- QuickSort.java
+++ QuickSort.java
@@ -1,6 +1,49 @@
 public class QuickSort {
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = low - 1; // index of smaller element
+
+        for(int j=low; j<high; j++){
+            // If current element is smaller than or equal to pivot
+            if(arr[j] <= pivot){
+                i++; //increment index of smaller element
+                //TODO:swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        i++; //increment index of smaller element
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i; //pivot index
+    }
+
+    public static void quickSort(int arr[], int low, int high){
+        if(low < high){
+            int pidx = partition(arr, low, high);
+
+            quickSort(arr, low, pidx - 1);
+            quickSort(arr, pidx + 1, high);
+        }
+    }
+
     public static void main(String[] args) {
+        int arr[] ={6, 3, 9, 5, 2, 8};
+        int n = arr.length;
+       
+        quickSort(arr, 0, n - 1);
+
+        //print
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
         
+        //TODO: time complexity: O(n log n)
+        //TODO: space complexity: O(log n)
     }
 }
 
