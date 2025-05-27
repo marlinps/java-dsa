99bbf847d54797dd7ec2e3af4f933fa2fbfeace0
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-27 15:20:52 +0700

    update OOPS

--- OOPS.java
+++ OOPS.java
@@ -11,7 +11,11 @@ class Pen{
 public class OOPS {
     public static void main(String[] args) {
         Pen pen1 = new Pen();
+        pen1.color = "Navy";
+        pen1.type = "Ballpoint";
+        
+        System.out.println("Pen Color " + pen1.color + ", types " + pen1.type);
+
         pen1.write();
-    }
-    
+    } 
 }
