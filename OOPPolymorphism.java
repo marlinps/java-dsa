fed4d6b2f66d612b07577244e67dcb9b78a8cba1
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-29 14:09:32 +0700

    create OOPPolymorphism

---
+++ OOPPolymorphism.java
@@ -0,0 +1,10 @@
+public class OOPPolymorphism {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/* Polymorphism in OOP
+ * Polymorphism is a core concept in Object-Oriented Programming (OOP) that allows objects to be treated as instances of their parent class, even if they are actually instances of a child class. This enables a single interface to represent different underlying forms (data types).
+ * 
+*/
