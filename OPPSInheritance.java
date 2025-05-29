ba93f4158e4bf1c74d35239af71e7475acfabebe
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-29 15:04:35 +0700

    update OPPSInheritance

--- OPPSInheritance.java
+++ OPPSInheritance.java
@@ -1,6 +1,25 @@
+class Shape{
+    public void area(){
+        System.out.println("displays area");
+    }
+}
+
+class Triangle extends Shape{
+    public void area(int l, int h){
+        System.out.println(1/2*l*h);
+    }
+}
+
+class Circle extends Shape{
+    public void area(int r){
+        System.out.println((3.14) * r * r);
+    }
+}
+
 public class OPPSInheritance {
-    
     public static void main(String[] args) {
-       
+       Triangle t1 = new Triangle();
+       t1.color = "Red";
+
     }
 }
