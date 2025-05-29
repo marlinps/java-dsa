c9c1ed0b93fe1f6eb0d0e0bda5c9bd61ed324670
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-29 16:58:56 +0700

    update  OPPSAccessModifier (add setter and getter)

--- bank/Bank.java
+++ bank/Bank.java
@@ -5,9 +5,23 @@ public class Bank {
     protected String email;
     private String password;
 
+    // Getter for private field
+    public String getPassword(){
+        return this.password;
+    }
+
+    // Setter for private field
+    private void setPassword(String password){
+        this.password = password; 
+    }
+
     public static void main(String[] args) {
         Bank account1 = new Bank();
         account1.name = "customer"; // Accessing public field
         account1.email = "email"; // Accessing protected field
-        account1.password = "password"; // Cannot access private field from outside the class
+        //account1.password = "password"; // Cannot access private field from outside the class
+        account1.setPassword("abcd");
+        System.out.println(account1.getPassword());
+
+    }
 }
