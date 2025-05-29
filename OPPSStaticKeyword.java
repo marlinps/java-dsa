b69eb4cd51a558487ae409e021d2ecedb2ef555d
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-29 18:17:15 +0700

    create OPPStaticKeyword

---
+++ OPPSStaticKeyword.java
@@ -0,0 +1,21 @@
+class Student {
+   String name;  
+   public static String school; 
+
+   public static void changeSchool(){
+         school = "Newschool";
+   }
+}
+
+
+
+public class OPPSStaticKeyword {
+    public static void main(String[] args) {
+       Student.school = "JMV";
+       Student s1 = new Student();
+      
+       s1.name = "Tony";
+      
+       System.out.println(s1.school);       System.out.println(Student.school);
+    } 
+}
