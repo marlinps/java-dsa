2401363c64457ad8ae82dcbfa184c0d6e83fc9f8
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-29 14:33:58 +0700

    create         s1.printInfo(s1.name, s1.age);

--- OOPPolymorphism.java
+++ OOPPolymorphism.java
@@ -1,6 +1,31 @@
+class Student{
+    String name;
+    int age;
+
+    public void prinInfo(String name){
+        System.out.println(name);
+    }
+
+    public void printInfo(int age){
+        System.out.println(age);
+    }
+
+    public void printInfo(String name, int age){
+        System.out.println(name + " " + age);
+    }
+}
+    
 public class OOPPolymorphism {
     public static void main(String[] args) {
-        
+        Student s1 = new Student();
+
+        s1.name = "Rayyan";
+        s1.age = 21;
+
+        // Method Overloading
+        s1.prinInfo(s1.name);
+        s1.printInfo(s1.age);
+        s1.printInfo(s1.name, s1.age);  
     }
 }
 
