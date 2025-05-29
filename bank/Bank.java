453be3499cd88a03369f40e7e849ae5853df3a97
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-29 15:43:02 +0700

    update  OPPSAccessModifier

--- bank/Bank.java
+++ bank/Bank.java
@@ -2,4 +2,12 @@ package bank;
 
 public class Bank {
     public String name;
+    protected String email;
+    private String password;
+
+    public static void main(String[] args) {
+        Bank account1 = new Bank();
+        account1.name = "customer"; // Accessing public field
+        account1.email = "email"; // Accessing protected field
+        account1.password = "password"; // Cannot access private field from outside the class
 }
