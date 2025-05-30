45c8c22ee7aaf422d42eaeda60895f361d809f5f
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-30 18:38:04 +0700

    update OPPSInterface

--- OOPSInterface.java
+++ OOPSInterface.java
@@ -5,12 +5,20 @@ interface Animal{
 }
 
 interface Herbivore{
+    void eat();
 
+    default void graze(){
+        System.out.println("Grazes on grass");
+    }
 }
 
 class Horse implements Animal, Herbivore{
     public void walk(){
         System.out.println("Walks on 4 legs");
+    }
+
+    public void eat(){
+        System.out.println("Eats grass");
     }
 }
 
@@ -18,6 +26,8 @@ public class OOPSInterface {
     public static void main(String[] args) {
         Horse horse = new Horse();
         horse.walk();
+        horse.eat();
+        horse.graze();
     }
    
 }
