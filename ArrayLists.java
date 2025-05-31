da5341d43f97fee7c93c312dc204b12fc1e39356
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-05-31 16:47:53 +0700

    update ArrayLists (create an ArrayLisr Oof integers and add some elements)

--- ArrayLists.java
+++ ArrayLists.java
@@ -2,7 +2,24 @@ import java.util.ArrayList;
 
 public class ArrayLists {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        // ArrayList<String> list2 = new ArrayList<String>();
+        // ArrayList<Boolean> list3 = new ArrayList<Boolean>();
+
+        // Add elements to the ArrayList
+        list.add(0);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+
     }
     
 }
+
+/* TODO: ArrayLists
+    * 1. Create an ArrayList of integers and add some elements to it.
+
+ * 
+ */
