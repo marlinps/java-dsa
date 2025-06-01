24a1616129d720fb90712d7f80433ae19e547960
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-06-01 17:25:29 +0700

    update LinkedLists(addFirst)

--- LinkedLists.java
+++ LinkedLists.java
@@ -1,12 +1,43 @@
+
 public class LinkedLists {
+    Node head; // Head of the linked list
+
+    class Node{
+        String data;
+        Node next;
+
+        // Constructor to create a new node
+        Node(String data){
+            this.data = data;
+            this.next = null; // Initially, the next node is null
+        }
+    }
+
+    // add - first, last
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head  = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+
 
     public static void main(String[] args) {
-        
+        LinkedLists list = new LinkedLists();
+        list.addFirst("a");
+        list.addFirst("is");
+
+       
     } 
 }
 
 /* TODO: LinkedLists is a data structure that consists of nodes, where each node contains a value and a reference to the next node in the sequence.
     * It allows for efficient insertion and deletion of elements, as well as dynamic resizing.
-    
+
  * 
  */
