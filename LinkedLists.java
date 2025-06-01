6dbe0f99fedeca2dae418d7a9a6bf64b602368b3
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-06-01 16:35:04 +0700

    create LinkedLists

---
+++ LinkedLists.java
@@ -0,0 +1,12 @@
+public class LinkedLists {
+
+    public static void main(String[] args) {
+        
+    } 
+}
+
+/* TODO: LinkedLists is a data structure that consists of nodes, where each node contains a value and a reference to the next node in the sequence.
+    * It allows for efficient insertion and deletion of elements, as well as dynamic resizing.
+    
+ * 
+ */
