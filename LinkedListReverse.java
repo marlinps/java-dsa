5b67d093fadda5d2a6a6849ac3a8d6437161df5d
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-06-05 14:49:49 +0700

    update LinkedListReverse.java

--- LinkedListReverse.java
+++ LinkedListReverse.java
@@ -7,5 +7,7 @@ public class LinkedListReverse {
 
 /* TODO: Reverse a LinkedList
   1. Create a LinkedList class with a Node inner class.
- * 
+ * input: 1 -> 2 -> 3 -> 4 -> 5
+ * output: 5 -> 4 -> 3 -> 2 -> 1
+
  */
