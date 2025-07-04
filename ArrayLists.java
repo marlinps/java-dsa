ec03dc555c60605559600e507119224f4cbda2c1
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-07-04 20:18:41 +0700

    update ArrayLists.java

--- ArrayLists.java
+++ ArrayLists.java
@@ -49,7 +49,5 @@ public class ArrayLists {
 /* TODO: ArrayLists is dynamic array that can grow and shrink in size.
  * It is part of the Java Collections Framework and provides methods to manipulate the elements.
     * 1. Create an ArrayList of integers and add some elements to it.
-    
-
  * 
  */
