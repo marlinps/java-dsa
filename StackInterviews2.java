673326d79595f51fea4c8ccde7bc65adf9b8f228
marlinps <marlinpurnamas92@gmail.com>
2025-07-08 10:43:55 +0700

    create StackInterview2(Reverse a Stack)

---
+++ StackInterviews2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class StackInterviews2 {
+    public static void pushAtBottom(int data, Stack<Integer> s){
+        // Base Case
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(data, s); // Recursive
+        s.push(top);
+    }
+
+    public static void main(String[] args){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(4, s);
+  
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    } 
+}
+
+/* TODO: Reverse a Stack
+example
+input
+3
+2
+1
+
+output
+1
+2
+3
+ */
