6c6f96ca991ff241edf60cf21305e448a97616d2
marlinps <marlinpurnamas92@gmail.com>
2025-07-08 10:20:36 +0700

    create StackPackages.java

---
+++ StackPackages.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class StackPackages{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Stack s = new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
