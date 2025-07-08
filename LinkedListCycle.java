e4d575a86f1599a5917c6320c217c1ea8732fc92
marlinps <marlinpurnamas92@gmail.com>
2025-07-08 09:11:04 +0700

    update StackClass.java

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -17,7 +17,23 @@ public class LinkedListCycle {
 
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+
+        ListNode hare = head; // fast
+        ListNode turtle = head; // slow
         
+        while(hare != null && hare.next != null){
+            hare = hare.next.next;
+            turtle = turtle.next; 
+
+            if(hare == turtle){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
--- StackClass.java
+++ StackClass.java
@@ -9,7 +9,7 @@ public class StackClass{
         }
     }
 
-    public class Stack{
+    public static class Stack{
         public static Node head;
 
         public static boolean isEmpty(){
@@ -28,11 +28,39 @@ public class StackClass{
             head = newNode;  
         }
 
-        
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
 
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            return head.data;
+        }
     }
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        
+        Stack s = new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
