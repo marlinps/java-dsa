e7872d3ab89b22eee2c1caba7ef350978d215318
marlinps <marlinpurnamas92@gmail.com>
2025-07-08 08:57:48 +0700

    update StackClass.java

--- StackClass.java
+++ StackClass.java
@@ -1,3 +1,38 @@
 public class StackClass{
-    
+    static class Node{
+        int data;
+        Node next;
+        
+        public Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public class Stack{
+        public static Node head;
+
+        public static boolean isEmpty(){
+            return head == null;
+        }
+
+        public static void push(int data){
+            Node newNode = new Node(data);
+            
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;  
+        }
+
+        
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
