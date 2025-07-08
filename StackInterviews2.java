5a5a1dc05edf84a313f587a6e90ff1889707551e
marlinps <marlinpurnamas92@gmail.com>
2025-07-08 10:51:13 +0700

    update StackInterview2(Reverse a Stack)

--- StackInterviews2.java
+++ StackInterviews2.java
@@ -13,13 +13,26 @@ public class StackInterviews2 {
         s.push(top);
     }
 
+    public static void reverse(Stack<Integer> s){
+        // Base Case
+        if(s.isEmpty()){
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s); // Recursive
+        pushAtBottom(top, s);
+
+    }
+
+
     public static void main(String[] args){
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
 
-        pushAtBottom(4, s);
+       reverse(s);
   
         while(!s.isEmpty()){
             System.out.println(s.peek());
