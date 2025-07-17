d6e1565c5a2d256e01e38c3c03d3890b53faa174
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-07-17 11:39:59 +0700

    update BacktrackingSudokuSolver.java

--- BacktrackingSudokuSolver.java
+++ BacktrackingSudokuSolver.java
@@ -92,7 +92,7 @@ public class BacktrackingSudokuSolver {
 /* TODO: Hard level problem
  * TODO: Sudoku Solver 
  * TODO: remember rule sudoku (1-9) in each row, column and 3x3 sub-box
- 
+
 Write a program to solve a Sudoku puzzle by filling the empty cells.
 
 A sudoku solution must satisfy all of the following rules:
