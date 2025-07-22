97cb95107c6e3ceaf94513468258e8944b71dba4
marlinps <marlinpurnamas92@gmail.com>
2025-07-22 12:48:10 +0700

    update BacktrackingPrintAllPermutations.java

--- BacktrackingPrintAllPermutations.java
+++ BacktrackingPrintAllPermutations.java
@@ -20,8 +20,7 @@ public class BacktrackingPrintAllPermutations {
     }
 }
 
-/*
-TODO: Print all permutations of a given string using backtracking.
+/*TODO: Print all permutations of a given string using backtracking.
  base case if the string is empty, print the current permutation
  For example, if the input string is "abc", the output should be:
  "abc", "acb", "bac", "bca", "cab", "cba"
