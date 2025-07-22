995eceaed34f8e15183679d092518251c5e73dcd
marlinps <marlinpurnamas92@gmail.com>
2025-07-22 12:51:13 +0700

    update Bits.java

--- Bits.java
+++ Bits.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Bits {
     public static void main(String[] args) {
-        //TODO: Get Bit -> AND (&)
+        // TODO: Get Bit -> AND (&)
         int n = 5;//0101
         int pos = 3;
         int bitMask = 1<<pos; // 1 << 3 = 8 (1000)
@@ -13,16 +13,16 @@ public class Bits {
             System.out.println("bit was one");
         }
 
-        //TODO: Set Bit -> OR (|)
-        int n1 = 5;//0101
+        // TODO: Set Bit -> OR (|)
+        int n1 = 5; //0101
         int pos1 = 1;
         int bitMask1 = 1<<pos1;
 
         int newNumber = bitMask1 | n1;
         System.out.println(newNumber);     
         
-        //TODO: Clear Bit -> AND With NOT
-        int n2 = 5;//0101
+        // TODO: Clear Bit -> AND With NOT
+        int n2 = 5; //0101
         int pos2 = 2;
         int bitMask2 = 1<<pos2;
         int notBitMask = ~(bitMask2);
@@ -30,11 +30,11 @@ public class Bits {
         int newNumber2 = notBitMask & n2;
         System.out.println(newNumber2);
 
-        //TODO: Update Bit
+        // TODO: Update Bit
         Scanner sc = new Scanner(System.in);
         int oper = sc.nextInt();
-        //oper=1 : set oper=0 : clear
-        int n3 = 5; //0101 -> 0111 -> dec 7
+        // oper=1 : set oper=0 : clear
+        int n3 = 5; // 0101 -> 0111 -> dec 7
         int pos3 = 1;
 
         int bitMask3 = 1<<pos3;
