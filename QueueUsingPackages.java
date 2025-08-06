2ac0b02e8c9d80eded436ba6cb05ca558cac3fc7
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-08-06 13:22:09 +0700

    create QueueUsingPackages.java

--- QueueUsingPackages.java
+++ QueueUsingPackages.java
@@ -1,5 +1,24 @@
+import java.util.*;
+
 public class QueueUsingPackages {
+    
+   
     public static void main(String[] args) {
-        
+        // Queue q = new Queue();
+        //Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while (!q.isEmpty()) {
+                System.out.println(q.peek());
+                q.remove();
+        }
     }
 }
+
+//  TODO: Queue using java collections
